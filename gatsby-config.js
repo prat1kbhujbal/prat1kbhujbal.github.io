6891fe3728898cd9018e57aca02f24aecfc71836
prat1kbhujbal <bhujbalpratik97@gmail.com>
2023-06-04 17:49:15 -0400

    added projects, robot images and project demo videos

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,6 +38,13 @@ module.exports = {
             },
         },
         {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+              name: `assets`,
+              path: `${__dirname}/src/assets/`,
+            },
+          },
+        {
             resolve: 'gatsby-source-filesystem',
             options: {
                 name: 'content',
