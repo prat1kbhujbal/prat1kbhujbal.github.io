b852b7e7c06413ba31c7aa436b90c169ede6b1e5
prat1kbhujbal <bhujbalpratik97@gmail.com>
2023-06-04 17:02:20 -0400

    added projects, robot images and project demo videos

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,6 +7,19 @@
 const path = require('path');
 const _ = require('lodash');
 
+exports.createSchemaCustomization = ({ actions }) => {
+    const { createTypes } = actions;
+    const typeDefs = `
+      type MarkdownRemark implements Node {
+        frontmatter: Frontmatter
+      }
+      type Frontmatter {
+        video: File
+      }
+    `;
+    createTypes(typeDefs);
+  };
+
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions;
   const postTemplate = path.resolve(`src/templates/post.js`);
